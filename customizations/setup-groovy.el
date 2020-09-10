a165f5fedd6e94bd3f2529e92361b8742603b1af
Lucija Vojnovic <lucija.vojnovic@gmail.com>
2020-09-10 15:02:47 +0200

    groovy mode for gradle files

--- customizations/setup-groovy.el
+++ customizations/setup-groovy.el
@@ -1,6 +1,7 @@
-;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+;;; use groovy-mode when file ends in .groovy, .gradle or has #!/bin/groovy at start
 (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
 
 (require 'darwin-mode)
